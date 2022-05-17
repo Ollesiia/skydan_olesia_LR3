7cd917ef671870e7276b2e6d74ff5e6774a6559a
Ollesiia <105593924+Ollesiia@users.noreply.github.com>
2022-05-17 11:51:27 +0300

    Add files via upload

---
+++ LR 3.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Laba_3._3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] scores = { 3,6,12,567, 456, 569, 35, 0};
+            foreach (var ig in scores)
+            {
+                Console.Write(ig + " ");
+            }
+
+
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score %2 !=0
+                orderby score
+                select score;
+            
+
+
+            foreach (int i in scoreQuery)
+            {
+                Console.Write("\n " + i + " ");
+            }
+        }
+
+    }
+}
